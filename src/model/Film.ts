3977816ac9095cd09fd6973cb7030fb28e0d1efa
Alvaro Alonso Domenech <a.alonsodomenech@campus.tu-berlin.de>
2020-12-14 19:28:55 +0100

    and and get users films

--- src/model/Film.ts
+++ src/model/Film.ts
@@ -5,14 +5,12 @@ export type FilmDocument = mongoose.Document & {
     name: string;
     released: Date;
     genre: string[];
-    userId: Schema.Types.ObjectId
 };
 
 const filmSchema = new mongoose.Schema({
     name: { type: String, unique: true, required: true },
     released: { type: Date, required: true},
     genre: [{ type: String }],
-    userId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 
 }, { timestamps: true });;
 
